d86cc6fc25d227bbbfb447e4e426004d69b13d33
alicewonders <32062286+alicewonders@users.noreply.github.com>
2018-05-23 00:19:40 +0700

    Add files via upload

--- src/LogoField.java
+++ src/LogoField.java
@@ -7,9 +7,9 @@ import java.io.IOException;
 
 public class LogoField extends JPanel {
     private final int PANEL_SIZE = 600;
-    private final int WINDOW_OFFSET = 20; //верхняя "крышка" окна съедает часть клетки, нужно сместится
-    private final int FIELD_DOWN_OFFSET = PANEL_SIZE / 6;
     private final int FIELD_LRU_OFFSET = PANEL_SIZE / 15;
+    private final int UP_OFFSET = 30;
+    private final int LEFT_OFFSET = 40;
 
     private Turtle turtle;
     private int fieldSize;
@@ -45,24 +45,11 @@ public class LogoField extends JPanel {
 //        System.out.println(cellHeight);
 //        System.out.println(cellWidth);
 
-        final int UP_OFFSET = 30;
-        final int LEFT_OFFSET = 40;
         int lineOffset = 500 / fieldSize;
         for (int i = 0; i <= fieldSize; i++) {
             g2.drawLine(LEFT_OFFSET + i * lineOffset, UP_OFFSET,
                     LEFT_OFFSET + i * lineOffset, UP_OFFSET + fieldSize * lineOffset); //vertical
             g2.drawLine(LEFT_OFFSET, UP_OFFSET + i * lineOffset, LEFT_OFFSET + fieldSize * lineOffset, UP_OFFSET + i * lineOffset);
         }
-
-//        for (int i = 0; i <= fieldSize; i++) {
-//            g2.drawLine(getWidth() - i * cellWidth - FIELD_LRU_OFFSET, WINDOW_OFFSET + FIELD_LRU_OFFSET,
-//                    getWidth() - i * cellWidth - FIELD_LRU_OFFSET, getHeight() - FIELD_DOWN_OFFSET); //vertical
-//            g2.drawLine(FIELD_LRU_OFFSET, getHeight() - FIELD_DOWN_OFFSET - i * cellHeight,
-//                    getWidth() - FIELD_LRU_OFFSET, getHeight() - FIELD_DOWN_OFFSET - i * cellHeight); //horizontal
-//        }
-//        g2.drawLine(FIELD_LRU_OFFSET, WINDOW_OFFSET + FIELD_LRU_OFFSET,
-//                FIELD_LRU_OFFSET, getHeight() - FIELD_DOWN_OFFSET);
-//        g2.drawLine(FIELD_LRU_OFFSET, WINDOW_OFFSET + FIELD_LRU_OFFSET,
-//                getWidth() - FIELD_LRU_OFFSET, WINDOW_OFFSET + FIELD_LRU_OFFSET);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
         Bground game = new Bground(10);
