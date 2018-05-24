ffcff6851834254a2f7f29ef85e1fb9a392c8914
alicewonders <32062286+alicewonders@users.noreply.github.com>
2018-05-24 17:28:30 +0700

    Add files via upload

--- src/LogoField.java
+++ src/LogoField.java
@@ -1,8 +1,11 @@
+import javafx.util.Pair;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class LogoField extends JPanel {
     private final int UP_OFFSET = 30;
@@ -10,6 +13,7 @@ public class LogoField extends JPanel {
     private final int GAME_FIELD_SIZE = 500;
 
     private Turtle turtle;
+    private ArrayList<Coordinates> turtleDrawHistory = new ArrayList<>();
     private int fieldSize;
     private int cellSize;
     private Image bg = null;
@@ -26,7 +30,7 @@ public class LogoField extends JPanel {
     LogoField(int size) {
         this.fieldSize = size;
         this.cellSize = GAME_FIELD_SIZE / fieldSize;
-        turtle = new Turtle(3 * cellSize + LEFT_OFFSET, 3 * cellSize + UP_OFFSET, cellSize, size);
+        turtle = new Turtle(3 * cellSize + LEFT_OFFSET, 3 * cellSize + UP_OFFSET, cellSize, size, turtleDrawHistory);
         this.setLayout(new BorderLayout());
         this.add(turtle);
     }
@@ -46,6 +50,14 @@ public class LogoField extends JPanel {
                     LEFT_OFFSET + i * lineOffset, UP_OFFSET + fieldSize * lineOffset); //vertical
             g2.drawLine(LEFT_OFFSET, UP_OFFSET + i * lineOffset,
                     LEFT_OFFSET + fieldSize * lineOffset, UP_OFFSET + i * lineOffset); //horizontal
+        }
+
+        g2.setColor(Color.PINK);
+        g2.setStroke(new BasicStroke(4.0f));
+        // Drawing path
+        for (Coordinates coordinates : turtleDrawHistory) {
+            g2.drawLine(coordinates.getX1() + cellSize / 2, coordinates.getY1() + cellSize / 2,
+                        coordinates.getX2() + cellSize / 2, coordinates.getY2() + cellSize / 2);
         }
     }
 }
--- src/Turtle.java
+++ src/Turtle.java
@@ -1,3 +1,5 @@
+import javafx.util.Pair;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -5,25 +7,32 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Turtle extends JPanel implements KeyListener {
     private final int UP_OFFSET = 30;
     private final int LEFT_OFFSET = 40;
     private final int GAME_FIELD_SIZE = 500;
 
+//    private int oldx, oldy, newx, newy;
     private int x = 0, y = 0, velx = 0, vely = 0, cell, cage;
     private boolean isDrawing;
+    private ArrayList<Coordinates> turtleDrawHistory = null;
 
-    public Turtle(int startX, int startY, int cellSize, int cageNum){
+    public Turtle(int startX, int startY, int cellSize, int cageNum, ArrayList<Coordinates> turtleDrawHistory){
         addKeyListener(this);
         setFocusable(true);
         setOpaque(false);
         setFocusTraversalKeysEnabled(false);
+        this.turtleDrawHistory = turtleDrawHistory;
         x = startX;
         y = startY;
+//        newx = x;
+//        newy = y;
         cell = cellSize;
         cage = cageNum;
         isDrawing = false;
+        repaint();
     }
 
     @Override
@@ -38,76 +47,63 @@ public class Turtle extends JPanel implements KeyListener {
             System.err.println("Error while opening image");
         }
         g.drawImage(turt, x, y, null);
+//        if (isDrawing) {
+//            g.setColor(Color.ORANGE);
+//            Graphics2D g2 = (Graphics2D) g;
+//            g2.setStroke(new BasicStroke(3.0f));
+//            g2.drawLine(oldx + cell / 2, oldy + cell / 2,
+//                        newx + cell / 2, newy + cell /2);
+//        }
     }
 
-    private void up() {
-        if (y < UP_OFFSET + cell) {
-            vely = (cage - 1) * cell;
-            velx = 0;
-        }
-        else {
-            velx = 0;
-            vely = -cell;
-        }
-    }
-    private void down() {
-        if (y > GAME_FIELD_SIZE - cell) {
-            vely = - (cage - 1) * cell;
-            velx = 0;
-        }
-        else {
-            velx = 0;
-            vely = cell;
-        }
-    }
-    private void left() {
-        if (x < LEFT_OFFSET + cell) {
-            velx = (cage - 1) * cell;
+    private void move(char direction, int offset) {
+        if (direction == 'L') {
             vely = 0;
+            velx = (x < LEFT_OFFSET + cell) ? (cage - offset) * cell : -cell;
         }
-        else {
-            vely = 0;
-            velx = -cell;
-        }
-    }
-    private void right() {
-        if (x > GAME_FIELD_SIZE - cell){
-            velx = - (cage - 1) * cell;
-            vely = 0;
-        }
-        else {
-            vely = 0;
-            velx = cell;
-        }
-    }
 
-    private void draw() {
+        if (direction == 'R'){
+            vely = 0;
+            velx = (x > GAME_FIELD_SIZE - cell) ? -(cage - offset) * cell : cell;
+        }
 
+        if (direction == 'D') {
+            velx = 0;
+            vely = (y > GAME_FIELD_SIZE - cell) ? -(cage - offset) * cell : cell;
+        }
+
+        if (direction == 'U') {
+            velx = 0;
+            vely = (y < UP_OFFSET + cell) ? (cage - offset) * cell : -cell;
+        }
+        int oldy = y;
+        int oldx = x;
+        y += vely;
+        x += velx;
+        if (isDrawing) {
+            turtleDrawHistory.add(new Coordinates(oldx, oldy, x, y));
+        }
     }
 
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
-
-        if (code == KeyEvent.VK_D){
-            if (!isDrawing) {
-                draw();
-            }
-            isDrawing = !isDrawing;
+        switch (code){
+            case KeyEvent.VK_D:
+                isDrawing = !isDrawing;
+                break;
+            case KeyEvent.VK_UP:
+                move('U', 1);
+                break;
+            case KeyEvent.VK_DOWN:
+                move('D', 1);
+                break;
+            case KeyEvent.VK_LEFT:
+                move('L', 1);
+                break;
+            case KeyEvent.VK_RIGHT:
+                move('R', 1);
+                break;
         }
-        if (code == KeyEvent.VK_UP) {
-            up();
-        }
-        if (code == KeyEvent.VK_DOWN) {
-            down();
-        }
-        if (code == KeyEvent.VK_LEFT) {
-            left();
-        }
-        if (code == KeyEvent.VK_RIGHT) {
-            right();
-        }
-        x += velx;
-        y += vely;
         repaint();
     }
 
