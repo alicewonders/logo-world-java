39c68f4de8a1b4e6057b17679e6ac40fbe048d32
alicewonders <32062286+alicewonders@users.noreply.github.com>
2018-06-05 12:20:54 +0700

    Add files via upload

--- src/lineEvent/CommandLine.java
+++ src/lineEvent/CommandLine.java
@@ -1,3 +1,5 @@
+package lineEvent;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -16,14 +18,11 @@ public class CommandLine extends JPanel implements EventListener{
         final JLabel commandLabel = new JLabel("Command:");
         commandLabel.setLayout(null);
         commandLabel.setFont(new Font("Courier New", Font.BOLD, 16));
-//        commandLabel.setBounds(117, 530, 70, 40);
         this.add(commandLabel);
 
         final JTextField commandInput = new JTextField(FIELD_COLUMNS);
         commandInput.setLayout(null);
         commandInput.setFont(new Font("Courier New", Font.PLAIN, 16));
-//        commandInput.setBackground(new Color(179, 240, 173));
-//        commandInput.setBounds(191, 540, 250, 20);
         this.add(commandInput);
 
         commandInput.addKeyListener(new KeyListener() {
@@ -38,9 +37,28 @@ public class CommandLine extends JPanel implements EventListener{
                 int code = e.getKeyCode();
                 switch (code) {
                     case KeyEvent.VK_ENTER:
-                        //return to the turtle
+                        fireCommandLineEvent(new CommandLineEvent(this, commandInput.getText().toUpperCase()));
                 }
             }
         });
+
+    }
+
+    private void fireCommandLineEvent(CommandLineEvent e) {
+        Object[] listeners = listenerList.getListenerList();
+
+        for (int i = 0; i < listeners.length; i +=2) {
+            if (listeners[i] == CommandLineListener.class) {
+                ((CommandLineListener)listeners[i + 1]).commandLineEventOccurred(e);
+            }
+        }
+    }
+
+    public void addCommandLineListener(CommandLineListener listener) {
+        listenerList.add(CommandLineListener.class, listener);
+    }
+
+    public void removeCommandLineListener(CommandLineListener listener) {
+        listenerList.remove(CommandLineListener.class, listener);
     }
 }
---
+++ src/lineEvent/CommandLineListener.java
@@ -0,0 +1,7 @@
+package lineEvent;
+
+import java.util.EventListener;
+
+public class CommandLineListener implements EventListener{
+    public void commandLineEventOccurred(CommandLineEvent e) {}
+}
